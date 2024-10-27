8093d322dcd9313430d37bb2ac7a5baf84b4630b
Jonas Lindinger <lindinger.jonas.yt@gmail.com>
2024-10-27 09:21:30 +0100

    Commented input scripts

--- Assets/_Project/Scripts/Network/Input/ClientInput.cs
+++ Assets/_Project/Scripts/Network/Input/ClientInput.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using LindoNoxStudio.Network.Player;
 using LindoNoxStudio.Network.Simulation;
 using Unity.Netcode;
@@ -11,37 +10,44 @@ namespace LindoNoxStudio.Network.Input
     [RequireComponent(typeof(PlayerInput))]
     public class ClientInput : NetworkBehaviour
     {
-        private const int InputBufferSize = 128; // 8 bit | 1 byte
+        // Count of Ticks we save
+        private const int InputBufferSize = 128;
         
         #if Client
+        // My Client Inputs
         private ClientInputState[] _localClientInputStates = new ClientInputState[InputBufferSize];
-
+        // The Tick of the first input we saved
         private uint _tickWeFirstStartedSavingInputs;
         
+        // References
         private PlayerInput _playerInput;
         
         #elif Server
+        // Client Inputs
         private ClientInputState[] _clientInputStates = new ClientInputState[InputBufferSize];
-        
         #endif
-
+        
+        // Calculated Buffer Size
         [HideInInspector] public int _bufferSize = 3;
         
         public override void OnNetworkSpawn()
         {
             #if Client
-            
+            // Referencing
             _playerInput = GetComponent<PlayerInput>();
-            
             #endif
         }
 
         #if Client
         
+        /// <summary>
+        /// Get's the current Input and save's it to list
+        /// </summary>
+        /// <param name="tick"></param>
         public void SaveInput(uint tick)
         {
             // Getting the current input state
-            ClientInputState inputForThisTick = GetClientInputState(tick, true);
+            ClientInputState inputForThisTick = GetClientInputState(tick);
             
             // Saving the input in the dictionary
             _localClientInputStates[tick % InputBufferSize] = inputForThisTick;
@@ -50,7 +56,10 @@ namespace LindoNoxStudio.Network.Input
             if (_tickWeFirstStartedSavingInputs == 0)
                 _tickWeFirstStartedSavingInputs = tick;
         }
-
+        
+        /// <summary>
+        /// Sends the last inputs to the Server
+        /// </summary>
         public void SendInputs()
         {
             // If we haven't saved any inputs yet, then we don't have anything to send
@@ -61,35 +70,56 @@ namespace LindoNoxStudio.Network.Input
             OnClientInputsRPC(GetInputsToSend(15));
         }
 
-        public ClientInputState GetClientInputState(uint tick, bool isCurrentTick = false)
+        /// <summary>
+        /// Returns an old ClientInputState or the CurrentCLientInputState. Depending on if we already have the input or not
+        /// </summary>
+        /// <param name="tick"></param>
+        /// <returns></returns>
+        public ClientInputState GetClientInputState(uint tick)
         {
-            ClientInputState clientInputState = new ClientInputState();
-            if (isCurrentTick)
+            // Check if we already have this ClientInputState. If we have, we return this.
+            ClientInputState clientInputState = _localClientInputStates[tick % InputBufferSize];
+            if (clientInputState != null)
             {
-                // Getting Inputs
-                Vector2 Cycle = _playerInput.actions["Cycle"].ReadValue<Vector2>();
-                float Pedals = _playerInput.actions["Pedals"].ReadValue<float>();
-                float Throttle = _playerInput.actions["Throttle"].ReadValue<float>();
-                
-                // Setting rotation
-                try
+                if (clientInputState.Tick == tick)
                 {
-                    PlayerController playerController = NetworkPlayer.LocalNetworkPlayer._playerController;
-                    clientInputState.SetUp(tick, Cycle, Pedals, Throttle, playerController.yaw);
-                }
-                catch (NullReferenceException e)
-                {
-                    clientInputState.SetUp(tick, Cycle, Pedals, Throttle, 0);
+                    return clientInputState;
                 }
             }
             else
             {
-                clientInputState = _localClientInputStates[tick % InputBufferSize];
+                clientInputState = new ClientInputState();                
+            }
+            
+            // We don't have the ClientInputState, so we return the current Input State
+            
+            // Getting Inputs
+            Vector2 cycle;
+            float pedals;
+            float throttle;
+            cycle = _playerInput.actions["Cycle"].ReadValue<Vector2>();
+            pedals = _playerInput.actions["Pedals"].ReadValue<float>();
+            throttle = _playerInput.actions["Throttle"].ReadValue<float>();
+
+            // Setting rotation
+            try
+            {
+                PlayerController playerController = NetworkPlayer.LocalNetworkPlayer._playerController;
+                clientInputState.SetUp(tick, cycle, pedals, throttle, playerController.yaw);
+            }
+            catch (NullReferenceException e)
+            {
+                clientInputState.SetUp(tick, cycle, pedals, throttle, 0);
             }
 
             return clientInputState;
         }
         
+        /// <summary>
+        /// Returns an array of ClientInputStates of the localClientInputStates. The array has the size of requestedSize. But If we don't have enough inputs, we return less inputs
+        /// </summary>
+        /// <param name="requestedSize">The ideal size of the array</param>
+        /// <returns></returns>
         private ClientInputState[] GetInputsToSend(int requestedSize)
         {
             // Calculate the current input count
@@ -119,6 +149,14 @@ namespace LindoNoxStudio.Network.Input
         }
         
         #elif Server
+        /// <summary>
+        /// Returns a input, the Server can use.
+        /// If we have the input that we want, we return the Input.
+        /// If we don't have Client Inputs, we return a no input Input
+        /// If we don't have the current Input, we return the last Input, that we used
+        /// </summary>
+        /// <param name="tick"></param>
+        /// <returns></returns>
         public ClientInputState GetClientInputState(uint tick)
         {
             ClientInputState clientInputState = new ClientInputState();
@@ -141,7 +179,11 @@ namespace LindoNoxStudio.Network.Input
             return clientInputState;
         }
         #endif
-
+        
+        /// <summary>
+        /// Remote Procedural Call: The Client Sends his inputs to the Server
+        /// </summary>
+        /// <param name="inputs"></param>
         [Rpc(SendTo.Server, Delivery = RpcDelivery.Unreliable)]
         private void OnClientInputsRPC(ClientInputState[] inputs)
         {
